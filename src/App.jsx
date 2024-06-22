799c1d95f887a3e3a40d1f95bed51b3c0ee2f82d
fahiz <fahizk100@gmail.com>
2024-06-22 10:11:30 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,129 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div>
+      <div className='flex justify-between mx-24 my-10 '>
+        <div>
+          <h1 className=' font-bold '>brand.io</h1>
+        </div>
+        <div className='md:flex  justify-start  gap-10 hidden md:block'>
+          <p>Home</p>
+          <p>About</p>
+          <p>Testimonies</p>
+          <p>Contact</p>
+
+        </div>
+        <div className='flex gap-2'>
+          <button className=' px-2 '>
+            Login
+          </button>
+          <button className=' bg-green-400 p-2 px-4'>
+            Sign Up
+          </button>
+        </div>
+      </div>
+
+      <div className=' flex  mx-20 fl'>
+        <div className='w-1/2  grid gap-8 '>
+          <h1 className='text-6xl font-bold mx-5  mt-20'>Tell a <span className='text-green-400'>better</span> <br />
+          brand  <span className='text-green-400'>story</span></h1>
+          <p className='me-32 mx-5 font-thin '>Literature surrounded insensible at indulgence or to admiration remarkably. Matter future lovers desire marked boy use. Chamber reached do he nothing be.</p>
+        <div>
+          <button className='p-2 px-5  bg-green-500 mx-5'>
+            Explore More
+          </button>
+          <button className='p-2 px-5  '>
+            Contact us
+          </button>
+        </div>
+        
+        </div>
+        <div className=''>
+           <div className=' absolute  bg-sky-300 rounded-full    m-16    h-90   '>
+              <img className=' relative    ' src="https://s3-alpha-sig.figma.com/img/3c27/927f/67daea87fb959731a55b792b163e8d7a?Expires=1719187200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=GdKvNr-P-CxE2~teeo~O66Q-kB1qF3Vzsznw7VtEsE6WqX0T~gw1pZH0Ns7Sl6IMR9uyCr~lK4-e668JgUiFFCSnvea0h4nHMc5PwQbAr3~SNzY~GxvwxlIs2jmKr7yq2Sn1sl21sYnx7ub9h6zI4xKDPsEo-qKvRSrdwB38s1LWU450nOx6lrBlUrE9QWh0v7GxLmRO7OETXdvy6xJSiVUq4U~yLhYg0BuAxFOS4KQ~uYCZ5ngtC8OBS~XB86V~yMvk1Lxf7A6cG8ANsLrhBwohWs5E47uvdtV0dZScB0ji4xLOuJPu8XurqmWDOkvJJqP6D5elhnPkrA9~kYqfkw__" alt="" />
+           </div>
+        </div>
+        
+      </div>
+     </div>
+     <div className='mx-24  mt-20 flex justify-between px-20 p-10  bg-gray-300 my-10'>
+        <p>planday</p>
+        <p>umbraco</p>
+        <p>Parelfisher</p>
+        <p>Brightpearal</p>
+        <p>planday</p>
+
+
+     </div>
+
+     <div className='flex  justify-center m-20'>
+      <h1 className='text-2xl  mx-20 flex text-center  px-10 w-2/6'>Grow faster with help your customers </h1>
+     
+     
+     
+     </div>
+<div className='flex justify-center mx-40 '>
+<div className='p-10 grid gap-5  '>
+<svg width="50"  className=' mx-28' height="59" viewBox="0 0 70 59" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M63.5376 0.0363915C59.9743 0.0363915 57.0752 2.93538 57.0752 6.49895V23.2624L55.8235 24.557C55.4954 23.798 55.0195 23.0895 54.3967 22.4766C51.8011 19.9224 47.6011 19.9455 45.0335 22.5288L35 32.6249L24.9663 22.5287C22.3992 19.9455 18.1989 19.922 15.6032 22.4764C14.9804 23.0892 14.5048 23.7978 14.1764 24.5569L12.9247 23.2623V6.49881C12.9247 2.93538 10.0257 0.0362549 6.46229 0.0362549C2.89885 0.0362549 0 2.93538 0 6.49881V26.9499C0 29.0573 0.570391 31.128 1.64951 32.938L13.835 50.8521L12.44 58.9961H57.56L56.165 50.8523L68.3505 32.9382C69.4296 31.128 70 29.0573 70 26.9499V6.49881C70 2.93538 67.101 0.0363915 63.5376 0.0363915ZM32.9492 54.8945H17.3039L18.1571 49.9133L5.14309 30.788C4.46154 29.6247 4.10156 28.2989 4.10156 26.9499V6.49881C4.10156 5.19711 5.16059 4.13782 6.46242 4.13782C7.76426 4.13782 8.82328 5.19698 8.82328 6.49881V24.9208L22.8184 39.3955L25.7668 36.5444L18.3252 28.8479C17.4754 27.847 17.5316 26.333 18.48 25.3999C19.4716 24.424 21.076 24.4332 22.057 25.4199L32.9492 36.3799V54.8945ZM65.8984 26.9499C65.8984 28.2988 65.5385 29.6245 64.8569 30.7879L51.8429 49.9133L52.6961 54.8945H37.0508V36.3799L47.9429 25.4199C48.9237 24.433 50.5282 24.4243 51.5199 25.3999C52.4683 26.333 52.5245 27.8469 51.6746 28.8479L44.233 36.5444L47.1815 39.3955L61.1767 24.921V6.49881C61.1767 5.19711 62.2357 4.13782 63.5376 4.13782C64.8394 4.13782 65.8984 5.19711 65.8984 6.49881V26.9499Z" fill="#313131"/>
+</svg>
+
+<h1 className='text-center'>Ratings &<br/> Reviews</h1>
+<p className='text-center'>Collect reviews, Q&A and other content from your customers started.</p>
+</div>
+<div className='p-10 grid gap-5'>
+<svg width="59" className=' mx-28' height="68" viewBox="0 0 59 68" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M56.8923 25.6063L33.6966 2.41052C31.2986 0.0124789 27.3964 0.0123423 24.9982 2.41052C22.8425 4.56616 22.6568 7.85383 24.2488 10.2014L24.2165 10.3629C22.5441 18.7253 18.4728 26.3328 12.4423 32.3632L1.80299 43.0025C-0.600793 45.4064 -0.601202 49.2969 1.80299 51.7008L7.60177 57.4996C10.0054 59.9034 13.8962 59.9035 16.3001 57.4996L17.7498 56.0499L27.8979 66.1979C30.3017 68.6018 34.1922 68.602 36.5961 66.1979C38.9943 63.7999 38.9943 59.8978 36.5961 57.4997L32.2469 53.1506L33.6967 51.7008C36.1005 49.2972 36.1007 45.4067 33.6967 43.0027L32.7115 42.0173C37.558 38.6241 43.0622 36.2618 48.9401 35.0861L49.1036 35.0534C51.5035 36.6737 54.7835 36.4132 56.8923 34.3044H56.8925C59.2903 31.9065 59.2903 28.0045 56.8923 25.6063ZM13.4007 54.6002C12.5995 55.4013 11.3023 55.4014 10.5012 54.6001L4.70238 48.8013C3.90108 47.9999 3.90108 46.7032 4.70238 45.9019L14.8503 35.7538L23.5486 44.4522C22.5558 45.445 14.418 53.5828 13.4007 54.6002ZM33.6966 60.399C34.4958 61.1983 34.4958 62.499 33.6966 63.2984C32.8973 64.0976 31.5964 64.0976 30.7972 63.2984L20.6491 53.1503L23.5486 50.2509L33.6966 60.399ZM26.448 47.3515C27.0643 46.7352 27.9869 45.7912 29.443 44.5476L30.7972 45.9019C31.5985 46.7032 31.5985 47.9999 30.7972 48.8013L29.3474 50.2511L26.448 47.3515ZM26.5351 41.64L17.6623 32.7672C22.4173 27.2768 25.8224 20.7956 27.6442 13.7546L45.5477 31.6582C38.5067 33.4798 32.0257 36.8849 26.5351 41.64ZM53.9925 31.4048C53.1912 32.206 51.8945 32.206 51.0932 31.4048L27.8976 8.2093C27.0963 7.40786 27.0963 6.11122 27.8976 5.30991C28.6989 4.5086 29.9959 4.5086 30.7972 5.30991L53.9925 28.5053C54.7918 29.3047 54.7918 30.6053 53.9925 31.4048Z" fill="#313131"/>
+</svg>
+<h1 className='text-center'>Sales and <br /> Marketing</h1>
+<p className='text-center'>Use your user-generated content in sales and marketing. </p>
+</div>
+<div className='p-10 grid gap-5'>
+<svg width="60" className=' mx-28' height="42" viewBox="0 0 70 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M67.9448 37.6354H63.8077V10.2726C63.8077 9.13942 62.8875 8.22077 61.7524 8.22077H50.4048C49.2697 8.22077 48.3496 9.13942 48.3496 10.2726V37.6354H42.7291V2.14347C42.7291 1.01033 41.809 0.0916748 40.6739 0.0916748H29.3262C28.1912 0.0916748 27.271 1.01033 27.271 2.14347V37.6354H21.6506V8.10122C21.6506 6.96807 20.7304 6.04942 19.5954 6.04942H8.24755C7.11249 6.04942 6.19233 6.96807 6.19233 8.10122V37.6354H2.05522C0.920163 37.6354 0 38.554 0 39.6872C0 40.8203 0.920163 41.739 2.05522 41.739H67.9448C69.0798 41.739 70 40.8203 70 39.6872C70 38.554 69.0798 37.6354 67.9448 37.6354ZM10.3028 37.6354V10.153H17.5401V37.6354H10.3028ZM31.3813 37.6354V4.19527H38.6187V37.6354H31.3813ZM52.46 37.6354V12.3244H59.6974V37.6354H52.46Z" fill="#313131"/>
+</svg>
+<h1 className='text-center'>Customer <br />Experience</h1>
+<p className='text-center'>In the end, it's all about your customers. Build their trust and help.</p>
+</div>
+</div>
+
+
+
+
+<div className=' flex  gap-10 justify-between mx-32 my-10'>
+<div className='w-2/6 grid gap-3'>
+    <h1 className='font-bold'>brand.io</h1>
+    <p>Nor again is there anyone who loves or pursues or desires to obtain pain of itself.</p>
+</div>
+
+<div className='w-3/6 grid gap-2'>
+<h1 className='font-bold'>About</h1>
+           <p>Home</p>
+          <p>About</p>
+          <p>Testimonies</p>
+          <p>Contact</p>
+</div>
+<div className='w-3/6 grid gap-2'>
+<h1 className='font-bold'>Privacy</h1>
+           <p>Privacy</p>
+          <p>Policy</p>
+          <p>Payment</p>
+          <p>terms</p>
+</div>
+<div className='w-1/2 grid gap-2'>
+<h1 className='font-bold'>Privacy</h1>
+           <p>Privacy</p>
+          <p>Policy</p>
+          <p>Payment</p>
+          <p>terms</p>
+</div>
+</div>
+    </>
+  )
+}
+
+export default App
